54273bb7519f5fa97babcc7a8266d9e6139b083f
Bruce Collie <brucecollie82@gmail.com>
2019-05-30 09:32:32 +0100

    Test that custom deleters work as expected

--- test/deleters.cpp
+++ test/deleters.cpp
@@ -15,19 +15,38 @@ struct nrc {
 
   int& c_;
 };
+
+struct nrc_delete {
+  nrc_delete() = default;
+
+  void operator()(nrc *n) const { n->c_--; }
+};
 // clang-format on
 
 TEST_CASE("counts don't get reset by default")
 {
   int count = 0;
   {
-    auto v = value_ptr<nrc>(new nrc(count));
+    auto v = make_val<nrc>(count);
   }
   REQUIRE(count == 1);
 
-  auto v2 = new value_ptr<nrc>(new nrc(count));
+  auto v2 = make_val<nrc>(count);
   REQUIRE(count == 2);
-  delete v2;
+}
 
-  REQUIRE(count == 2);
+TEST_CASE("counts get reset if using a custom deleter")
+{
+  int count = 0;
+  {
+    auto v = value_ptr<nrc, nrc_delete>(new nrc(count));
+    REQUIRE(count == 1);
+
+    auto v2 = v;
+    REQUIRE(count == 2);
+
+    auto v3 = std::move(v2);
+    REQUIRE(count == 2);
+  }
+  REQUIRE(count == 0);
 }
